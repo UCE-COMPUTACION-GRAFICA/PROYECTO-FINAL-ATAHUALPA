00eaaa67efda2f53e658d7a0e1fe7e5bef5eef07
Estefy2498 <55849752+Estefy2498@users.noreply.github.com>
2020-01-23 01:06:06 -0500

    actualiza

--- javascripts/Laberinto.js
+++ javascripts/Laberinto.js
@@ -7,17 +7,21 @@ crearLaberinto(){
     //forma de laberinto 1
 t.crear_texturas("texturas/cipresverde.jpg", 2, 2);
 
-cu.crear_cubo(-2400, 25, -220, 700, 90,100, textura, false, 0, 0, 0, 1, 1, 1);
+cu.crear_cubo(-2400, 35, -220, 700, 90,100, textura, false, 0, 0, 0, 1, 1, 1);
 
 //forma 2
-cu.crear_cubo(-2000, 25, 0, 550, 90,100, textura, false, 0, 0, 0, 1, 1, 1);
+cu.crear_cubo(-2000, 35, 0, 550, 90,100, textura, false, 0, 0, 0, 1, 1, 1);
 
 
 //forma 3
-cu.crear_cubo(-1800, 25, 220, 200, 90,150, textura, false, 0, 0, 0, 1, 1, 1);
+cu.crear_cubo(-1800, 35, 220, 200, 90,150, textura, false, 0, 0, 0, 1, 1, 1);
 
 //forma 4
-cu.crear_cubo(-1800, 25, 600, 250, 90,450, textura, false, 0, 0, 0, 1, 1, 1);
+cu.crear_cubo(-1850, 35, 600, 250, 90,450, textura, false, 0, 0, 0, 1, 1, 1);
+
+//forma 5
+cu.crear_cubo(-2200, 35, 600, 250, 90,450, textura, false, 0, 0, 0, 1, 1, 1);
+
 
 }
 
